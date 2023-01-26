89b17ad198eeb584f331bf05a7c2857d2b91aa83
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2023-01-26 10:29:26 +0100

    feat: task model and create task

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Task;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,51 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return 'Bienvenidos a mi aplicación de tareas';
+});
+
+// Crear tarea:
+Route::post("tasks", function (Request $request, Response $response) {
+    try {
+        // Recibimos del body la información
+        $title = $request->input('title');
+        $description = $request->input('description');
+
+        // Guardar en la bd:
+        $task = new Task();
+        $task->title = $title;
+        $task->description = $description;
+        $task->save();
+
+        // devolver resultado satisfactorio:
+        return response([
+            "success" => true,
+            "message" => "Task created successfully",
+            "data" => [
+                "task" => $task
+            ]
+        ], 200);
+
+    } catch (\Throwable $th) {
+        // Controlar el error
+        return response([
+            "success" => false,
+            "message" => "Cannot create task: " . $th->getMessage(),
+        ], 500);
+    }
+});
+
+Route::put("tasks", function () {
+    return "actualizar tarea";
+});
+
+Route::delete("tasks/{id}", function ($id) {
+    return "Eliminar tarea $id";
+});
+
+Route::get("tasks", function () {
+    return "todas las tareas";
+});
+
+Route::get("tasks/{id}", function ($id) {
+    return "Tarea numero $id";
 });
